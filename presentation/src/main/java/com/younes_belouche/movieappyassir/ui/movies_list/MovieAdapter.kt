730cda86fb84118d8484a5549442d43120d74eae
Younes <User@DESKTOP-NHH2UPS>
2022-12-06 10:22:04 +0100

    Changed project's structure

---
+++ presentation/src/main/java/com/younes_belouche/movieappyassir/ui/movies_list/MovieAdapter.kt
@@ -0,0 +1,14 @@
+package com.younes_belouche.movieappyassir.ui.movies_list
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.younes_belouche.domain.entities.Movie;
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.younes_belouche.movieappyassir.R
+
+class MovieAdapter{
+
+}
